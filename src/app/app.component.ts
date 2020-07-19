ed4b53dfb75679da6347bcec0eefe1b9a36e1623
ricsoft <42780925+ricsoft@users.noreply.github.com>
2020-07-19 13:19:02 -0700

    Add dynamodb query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as AWS from 'aws-sdk';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {
+  }
 
+  ngOnInit(): void {
+    AWS.config.region = environment.region;
+
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: environment.identityPoolId
+    });
+
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    const params = {
+      TableName: 'Webscraper',
+      KeyConditionExpression: '#ID = :ID',
+      ExpressionAttributeNames: {
+        '#ID': 'ID'
+      },
+      ExpressionAttributeValues: {
+        ':ID': 'Breweries'
+      }
+    };
+
+    dynamodb.query(params, (err, data) => {
+      if (err) {
+        console.log('Error', err);
+      } else {
+        console.log('Success', data.Items);
+      }
+    });
+  }
 }
