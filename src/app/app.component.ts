ee55acde62d20d17788c1700d4149434e343b51b
ricsoft <42780925+ricsoft@users.noreply.github.com>
2020-09-05 12:57:27 -0700

    Add Dageraad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,30 +14,36 @@ export class AppComponent implements OnInit {
   brewers: Array<BrewersObj> = [
     {
       index: 0,
+      name: 'Dageraad',
+      imageUrl: 'https://pbs.twimg.com/profile_images/1001512837137678336/sBmwWJDa_400x400.jpg',
+      isRounded: false
+    },
+    {
+      index: 1,
       name: 'Yellow Dog',
       imageUrl: 'https://yellowdogbeer.com/yellowpress/wp-content/uploads/2019/11/yellow-dog-brewing_home-page_collage-icon@2x.png',
       isRounded: false
     },
     {
-      index: 1,
+      index: 2,
       name: 'Moody Ales',
       imageUrl: 'http://www.moodyales.com/img/social-logo.jpg',
       isRounded: false
     },
     {
-      index: 2,
+      index: 3,
       name: 'Parkside',
       imageUrl: 'https://www.theparksidebrewery.com/wp-content/themes/parkside/images/circle-logo.png',
       isRounded: true
     },
     {
-      index: 3,
+      index: 4,
       name: 'Twin Sails',
       imageUrl: 'https://pbs.twimg.com/profile_images/813493559739359232/DhM6ZA8F_200x200.jpg',
       isRounded: true
     },
     {
-      index: 4,
+      index: 5,
       name: 'Fraser Mills',
       imageUrl: 'https://pbs.twimg.com/profile_images/921620804197957632/lztMFt4S_200x200.jpg',
       isRounded: false
